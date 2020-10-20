b39fd0dec18df6a52ba13170a86f55a0a11d7ffe
nikitagalayda <nikitagalayda@gmail.com>
2020-10-20 20:39:40 +0800

    add projectiles

--- Assets/Scripts/Players/SimpleTeleport.cs
+++ Assets/Scripts/Players/SimpleTeleport.cs
@@ -6,6 +6,8 @@ public class SimpleTeleport : MonoBehaviour
 {
     public float smoothTime = 0.05F;
     public float distanceThreshold = 0.01F;
+    // public float bulletSpeed;
+    public GameObject bulletPrefab;
 
     private bool inTransition = false;
     private Vector2 velocity = Vector2.zero;
@@ -21,22 +23,31 @@ public class SimpleTeleport : MonoBehaviour
         if (Input.GetMouseButtonDown(0))
         {
             // Debug.Log("sending out ray");
-            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
-            GameObject target = hit.collider.gameObject;
+            // RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+            // GameObject target = hit.collider.gameObject;
+            Vector2 currentMouseVector = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            GameObject bullet = Instantiate(bulletPrefab, transform.position, Quaternion.identity);
+            Vector2 shootDirection = (Vector2)Vector3.Normalize(currentMouseVector-(Vector2)transform.position);
+            Debug.Log(shootDirection);
+            bullet.GetComponent<ProjectileController>().moveToTarget(shootDirection);
+            bullet.GetComponent<ProjectileController>().parentObject = this.gameObject;
+            // bullet.transform.Translate(shootDirection * bulletSpeed * Time.deltaTime);
+            // bullet.GetComponent<Rigidbody2D>().velocity = shootDirection * bulletSpeed * Time.deltaTime;
+            // Debug.Log(bullet.GetComponent<Rigidbody2D>().velocity);
 
-            if (hit.collider != null)
-            {
+            // if (hit.collider != null)
+            // {
                 
-                if(!inTransition) {
-                    transitionTarget = target.transform.position;
-                    inTransition = true;
-                    target.GetComponent<ObjectController>().TransitionToPosition(transform.position);
-                }
-
-                // pseudocode
-                // if(target == someObjectType) {
+                // if(!inTransition) {
+                //     transitionTarget = target.transform.position;
+                //     inTransition = true;
+                //     target.GetComponent<ObjectController>().TransitionToPosition(transform.position);
                 // }
-            }
+
+            //     // pseudocode
+            //     // if(target == someObjectType) {
+            //     // }
+            // }
         }
 
         Vector2 current2DPosition = new Vector2(transform.position.x, transform.position.y);
@@ -50,6 +61,14 @@ public class SimpleTeleport : MonoBehaviour
     void FixedUpdate() {
         if(inTransition) {
             transform.position = Vector2.SmoothDamp(transform.position, transitionTarget, ref velocity, smoothTime);
+        }
+    }
+
+    public void TransitionToPosition(Vector2 targetPosition) {
+        if(!inTransition) {
+            transitionTarget = targetPosition;
+            // Debug.Log(transitionTarget);
+            inTransition = true;
         }
     }
 }

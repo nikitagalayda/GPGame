b9b945f7ce205c6cea5149c7f82795e072ed69c6
nikitagalayda <nikitagalayda@gmail.com>
2020-11-03 20:48:39 +0800

    add infinite background

---
+++ Assets/Scripts/Environment/Background.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background : MonoBehaviour
+{
+    public GameObject[] backgroundCards;
+    public float backgroundSpeed = 5f;
+
+    private LinkedList<GameObject> cardList;
+    private Vector3 screenBounds; 
+
+    void Start()
+    {
+        cardList = new LinkedList<GameObject>(backgroundCards);
+
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+    }
+
+    void Update()
+    {
+        transform.Translate(new Vector3(0, -backgroundSpeed, 0) * Time.deltaTime);
+    }
+
+    void LateUpdate()
+    {
+        GameObject lastBgCard = cardList.Last.Value;
+        GameObject firstBgCard = cardList.First.Value;
+
+        Vector3 cardExtents = lastBgCard.GetComponent<SpriteRenderer>().bounds.extents;
+
+        if((Mathf.Abs(lastBgCard.transform.position.y) - cardExtents.y) >= screenBounds.y) {
+            lastBgCard.transform.position = firstBgCard.transform.position + new Vector3(0, cardExtents.y*2, 0);
+            cardList.AddFirst(lastBgCard);
+            cardList.RemoveLast();
+        }
+    }
+
+}

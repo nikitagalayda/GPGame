37e8eb90b0dafb6bf803beaeef74dd9b6d6e5662
nikitagalayda <nikitagalayda@gmail.com>
2020-11-26 18:50:20 +0800

    fix teleport

--- Assets/Scripts/Objects/ProjectileController.cs
+++ Assets/Scripts/Objects/ProjectileController.cs
@@ -30,7 +30,7 @@ public class ProjectileController : MonoBehaviour
         movingToTarget = true;
     }
 
-    void OnTriggerEnter2D(Collider2D other)
+    void OnCollisionEnter2D(Collision2D other)
     {
         if (other.gameObject.tag == "Item")
         {

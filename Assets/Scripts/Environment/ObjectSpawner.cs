d8d8c64e5fb7dc7d14ebb9143a65f14c0dc986f6
nikitagalayda <nikitagalayda@gmail.com>
2020-12-26 20:30:26 +0800

    add meteorites

---
+++ Assets/Scripts/Environment/ObjectSpawner.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectSpawner : MonoBehaviour
+{
+    public float spawnCooldown;
+    public GameObject[] borders;
+    public GameObject spawnItem;
+
+    private float spawnMinLocation;
+    private float spawnMaxLocation;
+    private float nextSpawnTime = 1.0f;
+    private List<float> spawnLocations;
+    private float lastSpawnLocation = 0f;
+
+    void Start()
+    {
+        // TODO: add half of border width
+        spawnMinLocation = borders[0].transform.position.x;
+        spawnMaxLocation = borders[1].transform.position.x;
+        spawnLocations = new List<float> {-2f, -1f, 0f, 1f, 2f};
+    }
+
+    void Update()
+    {
+        if(Time.time >= nextSpawnTime) {
+            SpawnRandomItem();
+            nextSpawnTime = Time.time + spawnCooldown;
+        }
+    }
+
+    private void SpawnRandomItem() {
+        List<float> modList = new List<float>(spawnLocations);
+        modList.Remove(lastSpawnLocation);
+        
+        int randPos = Random.Range(0, modList.Count-1);
+        float spawnLocation = modList[randPos];
+        
+        Instantiate(spawnItem, new Vector3(spawnLocation, gameObject.transform.position.y, 0), Quaternion.identity);
+        lastSpawnLocation = spawnLocation;
+    }
+}

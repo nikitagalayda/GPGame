2a6a6e3b095a4a0628ab58852720c59d051e22e7
nannxnann <nannxnann@gmail.com>
2020-12-28 15:04:05 +0800

    finish main menu

--- Assets/Scripts/UI/MainMenuTextController.cs
+++ Assets/Scripts/UI/MainMenuTextController.cs
@@ -5,27 +5,39 @@ using UnityEngine.UI;
 
 public class MainMenuTextController : MonoBehaviour
 {
-    private float fontSize;
+    private int fontSize;
     private Text text;
     private static float t = 0.0f;
+    private int currSize;
 
     void Start()
     {
-        text = gameObject.GetComponent<Text>();
+        text = gameObject.GetComponentInChildren<Text>();
+
         fontSize = text.fontSize;
+        currSize = fontSize;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 
-    public void EnlargeText(int factor) {
-        Mathf.Lerp(fontSize, fontSize*factor, t);
+    IEnumerator EnlargeText(int newSize, float lerpTime)
+    {
+        Debug.Log("START COROUTINE");
+        float elapsedTime = 0f;
+        int startSize = fontSize;
+
+        do {
+            text.fontSize = (int)Mathf.Lerp(startSize, newSize, (elapsedTime/lerpTime));
+            elapsedTime += Time.deltaTime;
+            // Debug.Log(text.fontSize);
+            yield return null;
+        } while(elapsedTime < lerpTime);
     }
 
-    public void ReturnToNormalSize() {
-
+    public void ChangeTextSize(float factor, float lerpTime) {
+        StartCoroutine(EnlargeText((int)(fontSize*factor), lerpTime));
     }
 }
